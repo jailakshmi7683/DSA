1384f5c5fc5b2f5c3bad2dee7c79fa11d12b7f3f
jailakshmi7683 <kangulajailakshmi0314@gmail.com>
2026-01-16 13:08:19 +0530

    Creation of Circular LL

---
+++ CircularLL.java
@@ -0,0 +1,114 @@
+class CircularLL {
+    
+    Node head;
+    Node tail;
+    int size;
+    
+    CircularLL(){
+        size=0;
+    }
+    
+    class Node{
+        int data;
+        Node next;
+        
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    
+    //add-first
+    public void addFirst(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+            tail.next=head;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+        tail.next=newNode;
+    }
+    
+    //add-last
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        size++;
+        if(head==null){
+            head=newNode;
+            tail=newNode;
+            tail.next=head;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+        tail.next=head;
+    }
+    
+    //print-LL
+    public void printLL(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp=head;
+        do{
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }while(temp!=head);
+        System.out.println("HEAD");
+        
+    }
+    
+    //delete-first
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        size --;
+        if(head==tail){
+            head=null;
+            tail=null;
+            return;
+        }
+        head=head.next;
+        tail.next=head;
+    }
+    
+    //delete-last
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        size --;
+        if(head==tail){
+            head=null;
+            tail=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=tail){
+            temp=temp.next;
+        }
+        tail=temp;
+        tail.next=head;
+    }
+    
+    public static void main(String[] args) {
+        CircularLL list=new CircularLL();
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addLast(4);
+        list.printLL();
+        list.deleteFirst();
+        list.printLL();
+        list.deleteLast();
+        list.printLL();
+        
+    }
+}
